f958d9d7741de431bb2917c7d52b8052af37634d
sheikhsadi9494 <sadis6343@gmail.com>
2022-03-21 23:49:54 +0600

    commited by admin

---
+++ src/Pages/Home/Banner/Banner.js
@@ -0,0 +1,12 @@
+import { Box, Container} from '@mui/system';
+import React from 'react';
+
+const Banner = () => {
+    return (
+        <Container>
+            cd 
+        </Container>
+    );
+};
+
+export default Banner;
